0391f96e7b17d55e8bed4fabf35d9a0989d8c790
Tudor Teisanu <teisanutudort@gmail.com>
2022-12-15 23:56:59 +0200

    Added birthdate

--- src/mock.js
+++ src/mock.js
@@ -1,51 +1,62 @@
 const items = [
-    {
-        id: 1,
-        name: "John",
-        email: 'email@users.com',
-        avatar: 'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg'
-    },
-    {
-        id: 2,
-        name: "John",
-        email: 'email@users.com',
-        avatar: 'https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg'
-    },
-    {
-        id: 3,
-        name: "John",
-        email: 'email@users.com',
-        avatar: 'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector-PNG-File.png'
-    },
-    {
-        id: 4,
-        name: "John",
-        email: 'email@users.com',
-        avatar: 'https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png'
-    }
-]
-
+  {
+    id: 1,
+    name: "John",
+    email: "email@users.com",
+    avatar:
+      "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg",
+    birthdate: "2022-12-15T21:53:08.419Z",
+  },
+  {
+    id: 2,
+    name: "John",
+    email: "email@users.com",
+    avatar:
+      "https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg",
+    birthdate: "2022-12-15T21:53:08.419Z",
+  },
+  {
+    id: 3,
+    name: "John",
+    email: "email@users.com",
+    avatar:
+      "https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector-PNG-File.png",
+    birthdate: "2022-12-15T21:53:08.419Z",
+  },
+  {
+    id: 4,
+    name: "John",
+    email: "email@users.com",
+    avatar:
+      "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
+    birthdate: "2022-12-15T21:53:08.419Z",
+  },
+];
 
 const headers = [
-    {
-        text: 'ID',
-        value: 'id'
-    },
-    {
-        text: 'Name',
-        value: 'name'
-    },
-    {
-        text: 'Avatar',
-        value: 'avatar'
-    },
-    {
-        text: 'email',
-        value: 'email'
-    },
-    {
-        text: '',
-    },
-]
+  {
+    text: "ID",
+    value: "id",
+  },
+  {
+    text: "Name",
+    value: "name",
+  },
+  {
+    text: "Birthdate",
+    value: "birthdate",
+  },
+  {
+    text: "Avatar",
+    value: "avatar",
+  },
+  {
+    text: "email",
+    value: "email",
+  },
+  {
+    text: "",
+  },
+];
 
-export {headers, items}
+export { headers, items };
--- src/pages/users/UserCell.jsx
+++ src/pages/users/UserCell.jsx
@@ -1,8 +1,11 @@
-
 export const UserCell = ({ value, property }) => {
-    if (property === "avatar") {
-        return <img width={50} height={50} src={value} alt={""} />;
-    }
+  if (property === "avatar") {
+    return <img width={50} height={50} src={value} alt={""} />;
+  }
 
-    return <div>{value}</div>;
+  if (property === "birthdate") {
+    return new Date(value).toISOString().substring(0, 10);
+  }
+
+  return <div>{value}</div>;
 };
